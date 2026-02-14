f5043b933ffa53b4dd4f445e30a1ba3407ec1b21
Alirazat352 <alirazat0786@gmail.com>
2026-02-14 11:46:09 +0500

    My all files are added
    Thanks

---
+++ script.js
@@ -0,0 +1,18 @@
+const text = "Some wishes are too big... So I made them reality.";
+let index = 0;
+
+function type() {
+    if(index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, 50);
+    }
+}
+
+type();function openThis() {
+    alert("You opened it 😎");
+}
+
+function forYou() {
+    alert("This is specially for you 💙");
+}
